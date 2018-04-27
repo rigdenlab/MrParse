9c449e22d08513eba5bf7acf9dbb8e288cedda80
jmht <linucks42@gmail.com>
2018-04-27 15:03:04 +0100

    Got PHMMER working

--- mrparse/test.py
+++ mrparse/test.py
@@ -9,12 +9,18 @@ sys.path.insert(0, '/opt/mrbump-trunk/include/ccp4') # for MRBUMP_pdbmerge
 sys.path.insert(0, '/opt/mrbump-trunk/include/output') # for WriteLogfile
 sys.path.insert(0, '/opt/mrbump-trunk/include/initialisation')
 sys.path.insert(0, '/opt/mrbump-trunk/include/file_info')
+sys.path.insert(0, '/opt/mrbump-trunk/include/modelling') # for Align_score
+sys.path.insert(0, '/opt/mrbump-trunk/include/seq_align') # for Align_score
+sys.path.insert(0, '/opt/mrbump-trunk/include/structures') # for Matches
 sys.path.insert(0, '/opt/mrbump-trunk/include/tools')
 
 import MRBUMP_target_info
 # from mrbump-trunk/include/initialisation
 import MRBUMP_keywords
 import MRBUMP_initialise
+
+import Matches
+import MRBUMP_phmmer
 
 class Init(object):
     """Dummy init object for testing"""
@@ -25,6 +31,11 @@ class Init(object):
         self.ONLYMODELS = False
         self.search_dict = {}
         self.seqin = None
+
+# Need to create logs dir for setTargetMTZinfo
+logdir = 'logs'
+if not os.path.isdir(logdir):
+    os.mkdir(logdir)
 
 keywords = MRBUMP_keywords.Keywords()
 keywords.JOBID = 'jmht'
@@ -48,17 +59,62 @@ init.keywords.col_labels['SIGF'] = 'SIGFTOXD3'
 init.keywords.col_labels['FREER_FLAG'] = 'FreeR_flag'
 
 mr_search_dir = "."
-
-# Need to create logs dir for setTargetMTZinfo
-logdir = 'logs'
-if not os.path.isdir(logdir):
-    os.mkdir(logdir)
-
 target_info = MRBUMP_target_info.TargetInfo()
 target_info.setMattCoefLogfile(os.path.join(mr_search_dir, logdir, "matt_coef.log"))
-retcode = target_info.setTargetInfo(init, mr_search_dir)
 
+def test_PHMMER():
+    """"see structures/Matches.py"""
+
+    # init
+    mstat = Matches.Match_struct()
+    #mstat.FastaDB = os.path.join(os.environ["CCP4"], "share", "mrbump", "data", "pdb70_ATOMseqs.txt")
+    mstat.FastaDB = os.path.join(os.environ["CCP4"], "share", "mrbump", "data", "pdb70.txt")
+
+    retcode = target_info.setTargetInfo(init, mr_search_dir)
+
+    mstat.phmmerLogfile   = os.path.join(init.search_dir, "logs", "phmmer.log")
+    mstat.phmmerALNfile   = os.path.join(init.search_dir, "logs", "phmmerALN.log")
+    mstat.phmmerTBLOUT    = os.path.join(init.search_dir, "logs", "phmmerTblout.log")
+    mstat.phmmerDOMTBLOUT = os.path.join(init.search_dir, "logs", "phmmerDomTblout.log")
+    ph = MRBUMP_phmmer.Phmmer()
+    ph.runPhmmer(seqin=target_info.seqfile,
+                 seqdb=mstat.FastaDB,
+                 phmmerTblout=mstat.phmmerTBLOUT,
+                 phmmerDomTblout=mstat.phmmerDOMTBLOUT,
+                 targetSequence=target_info.sequence,
+                 logfile=mstat.phmmerLogfile,
+                 alnfile=mstat.phmmerALNfile,
+                 debug=True)
+    no_of_hits = len(ph.resultsList)
+    hitList = ph.resultsList
+    mstat.PHresultsDict = ph.resultsDict
+    for i in mstat.PHresultsDict.keys():
+        sys.stdout.write("%s %s %.2lf %.2lf\n" % (i,
+                                                  mstat.PHresultsDict[i].alignment,
+                                                  mstat.PHresultsDict[i].localSEQID,
+                                                  mstat.PHresultsDict[i].overallSEQID))
+        mstat.phmmerALNList.append((i, mstat.PHresultsDict[i].alignment))
+        #mstat.makeI2report("%s %s" % (i, mstat.PHresultsDict[i].alignment), mstat.ALN_report)
+    source = "PHR"
+    with open(os.path.join(init.search_dir, "logs", "phmmer.log"), "r") as f:
+        mstat.phmmerLogString = f.read()
+    init.search_dict["Sequence_align_list"] = mstat.phmmerALNList
+
+    # Assign the details of the domains in the target based on what has been found by Phmmer
+    target_info.targetDomainDict=ph.targetDomainDict
+    target_info.noDomains=len(ph.targetDomainDict.keys())
+
+def test_TargetInfo():
+    retcode = target_info.setTargetInfo(init, mr_search_dir)
+
+
+test_PHMMER()
 sys.exit()
+
+
+
+
+
 
 
 #Anisotropy Correction 	i = InputANO() 	r = runANO(i) 	ResultANO()
