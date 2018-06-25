10e6a523933642823ef16c3dae4dd7d9fe09f63d
linucks <linucks42@gmail.com>
2018-06-25 17:59:09 +0100

    playing around...

--- mrparse/test_bio.py
+++ mrparse/test_bio.py
@@ -1,21 +1,31 @@
 #!/usr/bin/env ccp4-python
-#
+import sys
 from Bio import AlignIO
 from Bio.SeqFeature import SeqFeature, FeatureLocation
 
+
+from Bio import SearchIO
+align_file = "/opt/mrbump/tests/phmmer.log"
+io = SearchIO.read(align_file, 'hmmer3-text')
+
+h1 = io['3f85_A']
+len(h1) == 3 # 3 domains
+
+#h1.hsps or h1.fragments
+hs = h1.hsps[0] # first domain
+
+
+
+sys.exit()
 align_file = "phmmerAlignment.log"
-
 alignment = AlignIO.read(align_file, "stockholm")
-
 
 # Use .letter_annotations for (e.g. resSeq) information
 # Use .annotations for random information
 # Use .features for more structure information - domains?
-
 rcrd = alignment[0]
 rcrd.letter_annotations['resseq'] = list(range(len(rcrd)))
 print rcrd.letter_annotations
-
 
 example_feature1 = SeqFeature(FeatureLocation(5, 18), type="domain", qualifiers={'foo' : 'bar1'})
 rcrd.features.append(example_feature1)
