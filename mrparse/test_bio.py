6c1212ae04062978f37a0147019eed0da205955b
linucks <linucks42@gmail.com>
2018-07-12 17:50:16 +0100

    First draft of code to parse targets

--- mrparse/test_bio.py
+++ mrparse/test_bio.py
@@ -2,21 +2,72 @@
 import sys
 from Bio import AlignIO
 from Bio.SeqFeature import SeqFeature, FeatureLocation
-
-
 from Bio import SearchIO
+
+sys.path.insert(0, '/opt/mrbump/include/seq_align')
+
+from MRBUMP_phmmer import PHHit, Domains
+import simpleSeqID
+
+# Compare 1bik
 align_file = "/opt/mrbump/tests/phmmer.log"
 io = SearchIO.read(align_file, 'hmmer3-text')
 
-h1 = io['3f85_A']
-len(h1) == 3 # 3 domains
 
-#h1.hsps or h1.fragments
-hs = h1.hsps[0] # first domain
+assert len(io) == 31
 
+included = io.hit_filter(lambda x: x.is_included)
+
+assert len(included) == 30
+
+resultsDict = {}
+targetSequence = 'FOO' # This needs to be passed in
+
+rank = 0
+for i, hit in enumerate(included):
+    for j, hsp in enumerate(hit.hsps):
+        rank += 1
+        ph = PHHit()
+        ph.rank = rank
+        ph.chainName = hsp.hit_id
+        name, chain = hsp.hit_id.split('_')
+        ph.pdbName = name
+        ph.chainID = chain
+        ph.score = hsp.bitscore
+        ph.evalue = hsp.evalue
+        ph.ndomains = len(hit)
+        
+        
+        start, stop = hsp.hit_range
+        ph.alnRange = "{}-{}".format(start + 1, stop)
+        start, stop = hsp.query_range
+        ph.tarRange = "{}-{}".format(start + 1, stop)
+        ph.tarExtent = hsp.query_span - 1
+        targetAlignment = "".join(hsp.aln[0].upper()) # assume the first Sequence is always the target
+        ph.targetAlignment = targetAlignment
+        alignment = "".join(hsp.aln[1].upper()) # assume the first Sequence is always the target
+        ph.alignment = alignment
+
+        local, overall = simpleSeqID.simpleSeqID().getPercent(alignment, targetAlignment, targetSequence)
+        ph.localSEQID = local
+        ph.overallSEQID = overall
+        
+        key = hit.id + str(hsp.domain_index)
+        resultsDict[key] = ph
+
+
+for k, v in resultsDict.items():
+    print k, v
+        
+
+
+# Biopython includes 1toc_R1, which is not in mrbump as below threshold
+# Filter on is_included
 
 
 sys.exit()
+ 
+
 align_file = "phmmerAlignment.log"
 alignment = AlignIO.read(align_file, "stockholm")
 
