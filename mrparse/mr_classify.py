f1631236556a1b523b181769e9a19d02bdd07673
jmht <linucks42@gmail.com>
2018-11-08 10:38:24 +0000

    split_sequence routine

--- mrparse/mr_classify.py
+++ mrparse/mr_classify.py
@@ -4,12 +4,12 @@ Created on 18 Oct 2018
 @author: jmht
 '''
 import os
+import math
 
 import numpy as np
 from ample.util.sequence_util import Sequence
 from pyjob import cexec
 from pyjob.script import EXE_EXT
-
 
 def run_deepcoil(seqin, name='foo'):
     seq = Sequence(fasta=seqin)
@@ -37,7 +37,49 @@ def parse_deepcoil(outfile):
 
 seqin = '../data/O75410.fasta'
 
-run_deepcoil(seqin)
+
+def split_sequence(sequence, chunk_size=500, overlap=100):
+    """Split a sequence into chunks of chunk_size that overlap by overlap"""
+    
+    def _split_sequence(sequence, chunk_size, overlap):
+        return [sequence[i:i+chunk_size] for i in range(0, len(sequence)-overlap, chunk_size-overlap)]
+    
+    if chunk_size < 1:
+        raise Exception("chunk size too small")
+    if overlap >= chunk_size:
+        raise Exception("overlap too large")
+    len_seq = len(sequence)
+    if len_seq <= chunk_size:
+        return [sequence]
+    remaining = len_seq % chunk_size
+    if remaining == 0:
+        chunked = _split_sequence(sequence, chunk_size, overlap)
+    else:
+        # As the overlap has to be constant we split the list into a set
+        # of chunk_size sized chunks, and 2 at the end that will be split
+        # into smaller, but equally sized chunks
+        nchunks = math.floor(len_seq / chunk_size)
+        cut = int(chunk_size * (nchunks - 1))
+        head = sequence[0:cut]
+        tail = sequence[cut - overlap:] # include the overlap from the previous sequence
+        head = _split_sequence(head, chunk_size, overlap)
+        tail = _split_sequence(tail, chunk_size, overlap)
+        chunked = head + tail
+    return chunked
+    
+
+# aa = list(range(23))
+# print(split_sequence1(aa, chunk_size=4, overlap=2))
+
+
+# seq = Sequence(fasta=seqin)
+# aa = seq.sequences[0]
+# num_aa = len(aa)
+# if num_aa >= 500:
+#     split_sequence(aa)
+# 
+# run_deepcoil(seqin)
+
 # seq = Sequence(fasta=seqin)
 # 
 # d1f = '/opt/DeepCoil/O75410_1_500.out'
