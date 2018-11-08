2b009f6d5b96f777f4b0c83067a7fefbf19b62bb
jmht <linucks42@gmail.com>
2018-11-08 12:23:07 +0000

    First draft of CC probability calculator

--- mrparse/mr_classify.py
+++ mrparse/mr_classify.py
@@ -9,34 +9,7 @@ import math
 import numpy as np
 from ample.util.sequence_util import Sequence
 from pyjob import cexec
-from pyjob.script import EXE_EXT
-
-def run_deepcoil(seqin, name='foo'):
-    seq = Sequence(fasta=seqin)
-    seq.headers = ['>{}'.format(name)]
-    fasta_in = name + '.fasta'
-    seq.write_fasta(fasta_in)
-    cmd = ['/Users/jmht/miniconda2/envs/py36/bin/python',
-            '/opt/DeepCoil/deepcoil.py',
-            '-i',
-            fasta_in]
-    try:
-        cexec(cmd)
-    except OSError as e:
-        print("Error running command:{}\n{}".format(cmd, e))
-    out_file = '{}.out'.format(name)
-    return out_file 
-
-def parse_deepcoil(outfile):
-    with open(outfile) as fh:
-        tuples = [line.split() for line in fh.readlines()]
-    aa, vals = zip(*tuples)
-    return np.array(aa), np.array(vals, dtype=np.float)
-        
-        
-
-seqin = '../data/O75410.fasta'
-
+from pyjob.exception import PyJobExecutionError
 
 def split_sequence(sequence, chunk_size=500, overlap=100):
     """Split a sequence into chunks of chunk_size that overlap by overlap"""
@@ -58,42 +31,102 @@ def split_sequence(sequence, chunk_size=500, overlap=100):
         # As the overlap has to be constant we split the list into a set
         # of chunk_size sized chunks, and 2 at the end that will be split
         # into smaller, but equally sized chunks
-        nchunks = math.floor(len_seq / chunk_size)
-        cut = int(chunk_size * (nchunks - 1))
+        nchunks = max(1, math.floor(len_seq / chunk_size))
+        cut = max(chunk_size, int(chunk_size * (nchunks - 1)))
         head = sequence[0:cut]
         tail = sequence[cut - overlap:] # include the overlap from the previous sequence
         head = _split_sequence(head, chunk_size, overlap)
         tail = _split_sequence(tail, chunk_size, overlap)
         chunked = head + tail
     return chunked
-    
-
-# aa = list(range(23))
-# print(split_sequence1(aa, chunk_size=4, overlap=2))
 
 
-# seq = Sequence(fasta=seqin)
-# aa = seq.sequences[0]
-# num_aa = len(aa)
-# if num_aa >= 500:
-#     split_sequence(aa)
-# 
-# run_deepcoil(seqin)
+def run_deepcoil(fasta_in):
+    with open(fasta_in) as f:
+        name = f.readline().strip()[1:]
+    cmd = ['/Users/jmht/miniconda2/envs/py36/bin/python',
+            '/opt/DeepCoil/deepcoil.py',
+            '-i',
+            fasta_in]
+    try:
+        # Need to sent env so that we don't inherit python2 environment
+        cexec(cmd, env={})
+    #except (OSError, PyJobExecutionError) as e:
+    except OSError as e:
+        print("Error running command:{}\n{}".format(cmd, e))
+    out_file = '{}.out'.format(name)
+    if not os.path.isfile(out_file):
+        raise RuntimeError("Deepcoil did not produce output file: %s" % out_file)
+    return out_file
 
-# seq = Sequence(fasta=seqin)
-# 
-# d1f = '/opt/DeepCoil/O75410_1_500.out'
-# aa1, vals1 = parse_deepcoil(d1f)
-# 
-# d2f = '/opt/DeepCoil/O75410_400_.out'
-# aa2, vals2 = parse_deepcoil(d2f)
-# 
-# print(aa1[-100:])
-# print(aa2[0:100])
-# 
-# print(vals1[400:500])
-# print(vals2[0:100])
 
-# First 5 of 2nd sequence are different (non-zero vs zero) - probably edge effect
-# Values differ in last ~25 places
-# Overlap by 100 and take 0:-50 of first and 50:- of second sequnce
+def parse_deepcoil(outfile):
+    with open(outfile) as fh:
+        tuples = [line.split() for line in fh.readlines()]
+    aa, vals = zip(*tuples)
+    return np.array(aa), np.array(vals, dtype=np.float)
+
+
+def write_fasta(aa, name, maxwidth=80):
+    fname = "%s.fasta" % name
+    with open(fname, 'w') as w:
+        w.write('>%s%s' % (name, os.linesep))
+        for chunk in range(0, len(aa), maxwidth):
+            w.write(aa[chunk:chunk + maxwidth] + os.linesep)
+        w.write(os.linesep)
+    return fname
+
+
+def join_probability_chunks(probabilities_list, aa_list, overlap):
+    cc_prob = []
+    aa_check = []
+    start = 0
+    # Skip last as we always take up to the end
+    for p, a in zip(probabilities_list[:-1], aa_list[:-1]):
+        p = list(p)
+        a = list(a)
+        inset = int(overlap / 2)
+        end = len(p) - inset
+        cc_prob += p[start:end]
+        aa_check += a[start:end]
+        start = inset
+        
+    # add last
+    cc_prob += list(probabilities_list[-1])[start:]
+    aa_check += list(aa_list[-1])[start:]
+    return cc_prob
+
+def run_deepcoil_on_chunks(chunks):
+    deepcoil_outputs = []
+    for i, chunk in enumerate(chunks):
+        name = 'foo%d' % i
+        fasta_in = write_fasta(chunk, name)
+        dout = run_deepcoil(fasta_in)
+        deepcoil_outputs.append(dout)
+    return deepcoil_outputs
+
+def parse_chunk_probabilites(deepcoil_outputs):
+    aa_list = []
+    probabilities_list = []
+    for outfile in deepcoil_outputs:
+        aa, probs = parse_deepcoil(outfile)
+        aa_list.append(aa)
+        probabilities_list.append(probs)
+    return probabilities_list, aa_list
+
+
+def coiled_coil_probabilities(seqin, chunk_size = 500, overlap = 100):
+    seq_aa = Sequence(fasta=seqin).sequences[0]
+    chunks = split_sequence(seq_aa, chunk_size=chunk_size, overlap=overlap)
+    deepcoil_outputs = run_deepcoil_on_chunks(chunks)
+    probabilities_list, aa_list = parse_chunk_probabilites(deepcoil_outputs)
+    return join_probability_chunks(probabilities_list, aa_list, overlap)
+
+
+seqin = '../data/O75410.fasta'
+probs = coiled_coil_probabilities(seqin)
+print("GOT PROBS ",len(probs), probs)
+# aa = list(range(7))
+# aa = '1234567'
+# print(split_sequence(aa, chunk_size=4, overlap=2))
+
