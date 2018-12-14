0e155aa343d0408a5b37c19dbf2f0ccfad5904ca
jmht <linucks42@gmail.com>
2018-12-14 19:52:49 +0000

    Refactoring for clarity

--- mrparse/mr_analyse.py
+++ mrparse/mr_analyse.py
@@ -22,28 +22,15 @@ logger = logging.getLogger(__name__)
 HTML_DIR = '/opt/MrParse/pfam'
 POLL_TIME = 1
 
-def write_html(html_out, html_data, template_file='multi_domains_template.html', template_dir='/opt/MrParse/pfam/'):
-    from jinja2 import Environment, FileSystemLoader
-    env = Environment( 
-        loader=FileSystemLoader(template_dir), 
-#             autoescape=select_autoescape(['html']) 
-        )                                                                                                                                                                          
-    template = env.get_template(template_file)
-    with open(html_out, 'w') as w:
-        w.write(template.render(html_data))
-        
-
-# def create_webpage(search_model_finder, classifier, hklinfo=None, html_dir=None):
-    
-
 
 def run(seqin, hklin=None):
     if not os.path.isfile(seqin):
         raise RuntimeError("Cannot find seqin file: %s" % seqin)
      
     # Find homologs and determine properties
-    smf = SearchModelFinder(seqin, hklin=hklin)
-    mrc = MrClassifier(seqin=seqin)
+    search_model_finder = SearchModelFinder(seqin, hklin=hklin)
+    classifier = MrClassifier(seqin=seqin)
+    hkl_info = None
     if hklin:
         if not os.path.isfile(hklin):
             raise RuntimeError("Cannot find hklin file: %s" % hklin)
@@ -54,50 +41,71 @@ def run(seqin, hklin=None):
         nproc = 3 if hklin else 2
         logger.info("Running on %d processors." % nproc)
         pool = multiprocessing.Pool(nproc)
-        smf_result = pool.apply_async(smf)
-        mrc_result = pool.apply_async(mrc)
-        if hklin:
+        smf_result = pool.apply_async(search_model_finder)
+        mrc_result = pool.apply_async(classifier)
+        if hkl_info:
             hklin_result = pool.apply_async(hkl_info)    
         pool.close()
         logger.debug("Pool waiting")
         pool.join()
         logger.debug("Pool finished")
         try:
-            smf = smf_result.get()
+            search_model_finder = smf_result.get()
         except Exception as e:
             logger.critical('SearchModelFinder was unsuccessful: %s' % e)
         try:
-            mrc = mrc_result.get()
+            classifier = mrc_result.get()
         except Exception as e:
             logger.critical('MrClassifier was unsuccessful: %s' % e)
-        if hklin:
+        if hkl_info:
             try:
                 hkl_info = hklin_result.get()
             except Exception as e:
                 logger.critical('HklInfo was unsuccessful: %s' % e)
     else:
-        smf()
-        mrc()
-        if hklin:
+        search_model_finder()
+        classifier()
+        if hkl_info:
             hkl_info()
     
-    pfam_data = mrc.pfam_dict()
-    pfam_data['regions'] = smf.pfam_dict()
+    html_out = create_webpage(search_model_finder,
+                              classifier,
+                              hkl_info=hkl_info,
+                              html_dir=HTML_DIR)
+    # only on Mac OSX
+    subprocess.Popen(['open', html_out])
+    return
+
+
+def create_webpage(search_model_finder,
+                   classifier,
+                   hkl_info=None,
+                   html_dir=None,
+                   html_filename='mrparse.html'):
+    pfam_data = classifier.pfam_dict()
+    pfam_data['regions'] = search_model_finder.pfam_dict()
       
     js_data = 'var pfam_json = %s;\n' % json.dumps(pfam_data)
     with open(os.path.join(HTML_DIR, 'data.js'), 'w') as w:
         w.write(js_data)
      
-    html_data = {'homolog_table' : smf.as_html()}
-    if hklin:
+    html_data = {'homolog_table' : search_model_finder.as_html()}
+    if hkl_info:
         html_data['hkl_info'] = hkl_info.as_html()
      
     with open(os.path.join(HTML_DIR, 'html_data.pkl'), 'w') as w:
         pickle.dump(html_data, w)
 
-    html_out = os.path.join(HTML_DIR, 'mrparse.html')
+    html_out = os.path.join(HTML_DIR, html_filename)
     write_html(html_out, html_data)
+    return html_out
 
-    # only on Mac OSX
-    subprocess.Popen(['open', html_out])
-    return
+def write_html(html_out, html_data, template_file='multi_domains_template.html', template_dir='/opt/MrParse/pfam/'):
+    from jinja2 import Environment, FileSystemLoader
+    env = Environment( 
+        loader=FileSystemLoader(template_dir), 
+#             autoescape=select_autoescape(['html']) 
+        )                                                                                                                                                                          
+    template = env.get_template(template_file)
+    with open(html_out, 'w') as w:
+        w.write(template.render(html_data))
