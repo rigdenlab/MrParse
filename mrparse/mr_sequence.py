586dd0d7d7c506ddf07c628fb489dad964572228
jmht <linucks42@gmail.com>
2019-06-11 22:07:45 +0100

    Updated for write

--- mrparse/mr_sequence.py
+++ mrparse/mr_sequence.py
@@ -10,6 +10,7 @@ from Bio.Seq import Seq
 from Bio import SeqIO
 from Bio.SeqUtils import molecular_weight
 from Bio.Alphabet import IUPAC
+from Bio.SeqRecord import SeqRecord
 
 SUFFIX_TO_TYPE = { 'fasta' : 'fasta' }
 
@@ -25,6 +26,7 @@ class Sequence(object):
             self._read_sequence_file(seq_file, sequence_type)
         elif sequence:
             self._bio_seq = Seq(sequence, IUPAC.protein)
+            self._bio_seq_record = SeqRecord(self._bio_seq)
         self.nresidues = len(self._bio_seq)
         self.sequence = str(self._bio_seq)
     
@@ -54,35 +56,9 @@ class Sequence(object):
     def _calculate_molecular_weight(self):
         self._molecular_weight = molecular_weight(self._bio_seq, 'protein')
     
-#     def write(self, seq_file, sequence_type=None):
-#         if sequence_type is None:
-#             sequence_type = self._sequence_type_from_filename(seq_file)
-#             if not sequence_type:
-#                 raise RuntimeError("Cannot determine sequence type from file: {}".format(seq_file))
-
-# def write_fasta(aa, name, maxwidth=80):
-#     fname = "%s.fasta" % name
-#     with open(fname, 'w') as w:
-#         w.write('>%s%s' % (name, os.linesep))
-#         for chunk in range(0, len(aa), maxwidth):
-#             w.write(aa[chunk:chunk + maxwidth] + os.linesep)
-#         w.write(os.linesep)
-#     return fname
-# 
-# def read_fasta(fasta_file):
-#     _, ext = os.path.splitext(fasta_file)
-#     seq = ""
-#     with open(fasta_file) as fh:
-#         for i, line in enumerate(fh.readlines()):
-#             if i == 0:
-#                 assert line[0] == '>'
-#                 continue
-#             if i == 1 and ext.lower() == '.seq':
-#                 continue # seq files have a blank line 
-#             line = line.strip()
-#             if not line or line[0] == '>':
-#                 break
-#             seq += line
-#     return seq
-                
-            
+    def write(self, seq_file, sequence_type=None):
+        if sequence_type is None:
+            sequence_type = self._sequence_type_from_filename(seq_file)
+            if not sequence_type:
+                raise RuntimeError("Cannot determine sequence type from file: {}".format(seq_file))
+        SeqIO.write(self._bio_seq_record, seq_file, sequence_type)
--- tests/test_mr_sequence.py
+++ tests/test_mr_sequence.py
@@ -2,20 +2,48 @@
 import set_mrparse_path
 import conftest
 
+import os
 from mrparse.mr_sequence import Sequence
+from Bio import SeqIO
+from Bio.Alphabet import IUPAC
+
 
 TWOUVO_SEQ = 'ERCGEQGSNMECPNNLCCSQYGYCGMGGDYCGKGCQNGACWTSKRCGSQAGGATCTNNQCCSQYGYCGFGAEYCGAGCQGGPCRADIKCGSQAGGKLCPNNLCCSQWGFCGLGSEFCGGGCQSGACSTDKPCGKDAGGRVCTNNYCCSKWGSCGIGPGYCGAGCQSGGCDG'
+
+
 def test_2uvo(test_data):
     seq_info = Sequence(seq_file=test_data.x2uvoA_fasta)
     assert seq_info.nresidues == 171
     assert seq_info.sequence == TWOUVO_SEQ
     assert abs(seq_info.molecular_weight - 17131) < 0.1
     
-def test_2uvo_seq(test_data):
+
+def test_2uvo_write(test_data):
+    seq_info = Sequence(seq_file=test_data.x2uvoA_fasta)
+    filename = 'foo.fasta'
+    seq_info.write(filename)
+    assert os.path.isfile(filename)
+    s = SeqIO.read(filename, 'fasta', alphabet=IUPAC.protein)
+    assert len(s) == 171
+    os.unlink(filename)
+    
+
+def test_2uvo_seq():
     seq_info = Sequence(sequence=TWOUVO_SEQ)
     assert seq_info.nresidues == 171
     assert seq_info.sequence == TWOUVO_SEQ
     assert abs(seq_info.molecular_weight - 17131) < 0.1
+
+
+def test_2uvo_seq_write(test_data):
+    seq_info = Sequence(sequence=TWOUVO_SEQ)
+    filename = 'foo.fasta'
+    seq_info.write(filename)
+    assert os.path.isfile(filename)
+    s = SeqIO.read(filename, 'fasta', alphabet=IUPAC.protein)
+    assert len(s) == 171
+    os.unlink(filename)
+
 
 if __name__ == '__main__':
     import sys
