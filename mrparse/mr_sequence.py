61791fceb276b7f2d3a00823c42b46c04b8d80d2
jmht <linucks42@gmail.com>
2019-06-11 21:52:53 +0100

    Updated Sequence to init from sequences

--- mrparse/mr_sequence.py
+++ mrparse/mr_sequence.py
@@ -6,6 +6,7 @@ Created on 17 Nov 2018
 
 import os
 
+from Bio.Seq import Seq
 from Bio import SeqIO
 from Bio.SeqUtils import molecular_weight
 from Bio.Alphabet import IUPAC
@@ -15,16 +16,25 @@ SUFFIX_TO_TYPE = { 'fasta' : 'fasta' }
 
 class Sequence(object):
     """Class for handling sequence data"""
-    def __init__(self, seq_file, sequence_type=None):
+    def __init__(self, seq_file=None, sequence=None, sequence_type=None):
         self.sequence_file = seq_file
         self._molecular_weight = None
+        self._bio_seq = None
+        self._bio_seq_record = None
+        if seq_file:
+            self._read_sequence_file(seq_file, sequence_type)
+        elif sequence:
+            self._bio_seq = Seq(sequence, IUPAC.protein)
+        self.nresidues = len(self._bio_seq)
+        self.sequence = str(self._bio_seq)
+    
+    def _read_sequence_file(self, seq_file, sequence_type):
         if sequence_type is None:
             sequence_type = self._sequence_type_from_filename(seq_file)
             if not sequence_type:
                 raise RuntimeError("Cannot determine sequence type from file: {}".format(seq_file))
         self._bio_seq_record = SeqIO.read(seq_file, sequence_type, alphabet=IUPAC.protein)
-        self.nresidues = len(self._bio_seq_record)
-        self.sequence = str(self._bio_seq_record.seq)
+        self._bio_seq = self._bio_seq_record.seq
         
     @staticmethod
     def _sequence_type_from_filename(seq_file):
@@ -42,7 +52,7 @@ class Sequence(object):
         return self._molecular_weight
     
     def _calculate_molecular_weight(self):
-        self._molecular_weight = molecular_weight(self._bio_seq_record.seq, 'protein')
+        self._molecular_weight = molecular_weight(self._bio_seq, 'protein')
     
 #     def write(self, seq_file, sequence_type=None):
 #         if sequence_type is None:
--- tests/test_mr_sequence.py
+++ tests/test_mr_sequence.py
@@ -4,10 +4,17 @@ import conftest
 
 from mrparse.mr_sequence import Sequence
 
+TWOUVO_SEQ = 'ERCGEQGSNMECPNNLCCSQYGYCGMGGDYCGKGCQNGACWTSKRCGSQAGGATCTNNQCCSQYGYCGFGAEYCGAGCQGGPCRADIKCGSQAGGKLCPNNLCCSQWGFCGLGSEFCGGGCQSGACSTDKPCGKDAGGRVCTNNYCCSKWGSCGIGPGYCGAGCQSGGCDG'
 def test_2uvo(test_data):
-    seq_info = Sequence(test_data.x2uvoA_fasta)
+    seq_info = Sequence(seq_file=test_data.x2uvoA_fasta)
     assert seq_info.nresidues == 171
-    assert seq_info.sequence == 'ERCGEQGSNMECPNNLCCSQYGYCGMGGDYCGKGCQNGACWTSKRCGSQAGGATCTNNQCCSQYGYCGFGAEYCGAGCQGGPCRADIKCGSQAGGKLCPNNLCCSQWGFCGLGSEFCGGGCQSGACSTDKPCGKDAGGRVCTNNYCCSKWGSCGIGPGYCGAGCQSGGCDG'
+    assert seq_info.sequence == TWOUVO_SEQ
+    assert abs(seq_info.molecular_weight - 17131) < 0.1
+    
+def test_2uvo_seq(test_data):
+    seq_info = Sequence(sequence=TWOUVO_SEQ)
+    assert seq_info.nresidues == 171
+    assert seq_info.sequence == TWOUVO_SEQ
     assert abs(seq_info.molecular_weight - 17131) < 0.1
 
 if __name__ == '__main__':
