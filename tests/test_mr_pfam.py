bdb84ca3991878744bc585f92b9ea83cc8ccca0a
jmht <linucks42@gmail.com>
2019-06-12 23:03:00 +0100

    Fix other unit tests

--- tests/test_mr_pfam.py
+++ tests/test_mr_pfam.py
@@ -5,59 +5,30 @@ Created on 26 Oct 2018
 @author: jmht
 '''
 import set_mrparse_path
-from mrparse.mr_hit import find_hits
+from mrparse.mr_sequence import Sequence
 from mrparse.mr_homolog import homologs_from_hits
 from mrparse.mr_region import RegionFinder
 from mrparse.mr_pfam import add_pfam_dict_to_homologs
 
 
-
-def test_homologs(test_data):
-    seqin = test_data.x2uvoA_fasta
-    hits = find_hits(seqin)
+def test_homologs(test_data, get_2uvo_test_hits):
+    seq_info = Sequence(test_data.x2uvoA_fasta)
+    hits = get_2uvo_test_hits
     regions = RegionFinder().find_regions_from_hits(hits)
     homologs = homologs_from_hits(hits)
-    seqlen = len(read_fasta(seqin))
-    add_pfam_dict_to_homologs(regions, seqlen)
+    add_pfam_dict_to_homologs(regions, seq_info.nresidues)
     
-    print(homologs.values()[0])
-    print(homologs.values()[0]._pfam_json)
+    homolog = homologs.values()[0]
+    ali_start = homolog._sequence_hit.alnStart
+    pfam_ali_start = homolog._pfam_json['regions'][0]['aliStart']
+    assert ali_start == pfam_ali_start
+#     {'regions': [{'startStyle': 'curved', 'end': 79, 'start': 2, 'aliStart': 2, 
+#                   'text': '1iqb_B_1', 'endStyle': 'curved', 'colour': '#7f7f3f', 
+#                   'aliEnd': 79, 'metadata': 
+#                   {'start': 2, 'end': 79, 'description': 'Homolog 1iqb_B_1 from region #2', 
+#                    'database': 'PHMMER search'}}], 'length': 171}
     
 
-# def test_local():
-#     seqin = "/opt/MrParse/data/Q13586.fasta"
-#     topcons_dir = "/opt/MrParse/data/Q13586/topcons"
-#     annotation = get_annotation(seqin, topcons_dir=topcons_dir)
-#     annotation_chunks = get_annotation_chunks(annotation)
-#     class_d = pfam_classification_dict(annotation_chunks, len(annotation))
-#     assert class_d['length'] == 685    
-#     assert len(class_d['regions']) == 7
-#     
-#     jpred_rundir = "/opt/MrParse/data/Q13586/jpred"
-#     sspred = JPred().secondary_structure_prediction(jpred_rundir=jpred_rundir)
-#     sspred_chunks = get_annotation_chunks(sspred)
-#     sspred_d = pfam_classification_dict(sspred_chunks, len(sspred))
-#      
-#     assert sspred_d['length'] == 685    
-#     assert len(sspred_d['regions']) == 23
-# 
-# 
-# def test_remote():
-#     seqin = "/opt/MrParse/data/Q13586.fasta"
-#     annotation = get_annotation(seqin)
-#     annotation_chunks = get_annotation_chunks(annotation)
-#     class_d = pfam_classification_dict(annotation_chunks, len(annotation))
-#     assert class_d['length'] == 685    
-#     assert len(class_d['regions']) == 7
-#      
-#     sspred = JPred().secondary_structure_prediction(seqin=seqin)
-#     sspred_chunks = get_annotation_chunks(sspred)
-#     sspred_d = pfam_classification_dict(sspred_chunks, len(sspred))
-#       
-#     assert sspred_d['length'] == 685    
-#     assert len(sspred_d['regions']) == 23
-#     
-#     
 if __name__ == '__main__':
     import sys
     import pytest
