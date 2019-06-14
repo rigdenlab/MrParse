a406039cf2e4a863af93fa674bd6d9bfea0f9113
hlasimpk <hlasimpk@liv.ac.uk>
2019-06-14 11:42:49 +0100

    Changed how multiple sequences are handled

--- mrparse/mr_sequence.py
+++ mrparse/mr_sequence.py
@@ -1,8 +1,8 @@
-'''
+"""
 Created on 17 Nov 2018
 
-@author: jmht
-'''
+@author: jmht & hlasimpk
+"""
 
 import copy
 import os
@@ -14,6 +14,10 @@ from Bio.Alphabet import IUPAC
 from Bio.SeqRecord import SeqRecord
 
 SUFFIX_TO_TYPE = {'fasta': 'fasta'}
+
+
+class MultipleSequenceException(Exception):
+    pass
 
 
 class Sequence(object):
@@ -30,48 +34,43 @@ class Sequence(object):
             self._bio_seq_record = SeqRecord(self._bio_seq)
         self.nresidues = len(self._bio_seq)
         self.sequence = str(self._bio_seq)
-    
+        self.sequence = str(self._bio_seq)
+
     def __len__(self):
         if isinstance(self.nresidues, int):
             return self.nresidues
         return 0
-    
+
     def _read_sequence_file(self, seq_file, sequence_type):
         if sequence_type is None:
-            sequence_type = self._sequence_type_from_filename(seq_file)
+            sequence_type = self.sequence_type_from_filename(seq_file)
             if not sequence_type:
                 raise RuntimeError("Cannot determine sequence type from file: {}".format(seq_file))
 
-        self._bio_seq = ""
-        identifier = []
-        previous_seqs = []
-        for seq in SeqIO.parse(seq_file, sequence_type, alphabet=IUPAC.protein):
-            if seq.seq in previous_seqs:
-                continue
-            self._bio_seq += seq.seq
-            identifier.append(seq.id)
-            previous_seqs.append(seq.seq)
-        self._bio_seq_record = SeqRecord(self._bio_seq)
-        self._bio_seq_record.id = "||".join(identifier)
-        
+        try:
+            self._bio_seq_record = SeqIO.read(seq_file, sequence_type, alphabet=IUPAC.protein)
+            self._bio_seq = self._bio_seq_record.seq
+        except ValueError:
+            raise MultipleSequenceException
+
     @staticmethod
-    def _sequence_type_from_filename(seq_file):
+    def sequence_type_from_filename(seq_file):
         _, suffix = os.path.splitext(seq_file)
         suffix = suffix.lstrip('.').lower()
         try:
             return SUFFIX_TO_TYPE[suffix]
         except KeyError:
             return None
-    
+
     @property
     def molecular_weight(self):
         if self._molecular_weight is None:
             self._calculate_molecular_weight()
         return self._molecular_weight
-    
+
     def _calculate_molecular_weight(self):
         self._molecular_weight = molecular_weight(self._bio_seq, 'protein')
-    
+
     def write(self, seq_file, sequence_type=None, description=None):
         """Write sequence out to file seq_file of type sequence_type.
 
@@ -95,3 +94,46 @@ class Sequence(object):
         else:
             seq_record = self._bio_seq_record
         SeqIO.write(seq_record, seq_file, sequence_type)
+
+
+def merge_multiple_sequences(seq_file):
+    """
+    Function to merge multiple sequences from a fasta file
+
+    Parameters
+    ----------
+    seq_file : str
+        The filename of the file
+
+    Returns
+    -------
+    seq : :obj:
+        mrparse.mr_sequence.Sequence object
+
+    Raises
+    ------
+    RuntimeError
+    """
+
+    sequence_type = Sequence.sequence_type_from_filename(seq_file)
+    if not sequence_type:
+        raise RuntimeError("Cannot determine sequence type from file: {}".format(seq_file))
+
+    sequence = ""
+    identifier = []
+    previous_seqs = []
+    for seq in SeqIO.parse(seq_file, sequence_type, alphabet=IUPAC.protein):
+        if seq.seq in previous_seqs:
+            continue
+        sequence += seq.seq
+        identifier.append(seq.id)
+        previous_seqs.append(seq.seq)
+
+    bio_seq_record = SeqRecord(sequence)
+    bio_seq_record.id = "||".join(identifier)
+
+    file_name = os.path.basename(seq_file).split('.')[0]
+    merged_seq_file = os.path.join(os.getcwd(), '{}_merged.fasta'.format(file_name))
+    SeqIO.write(bio_seq_record, merged_seq_file, "fasta")
+
+    return Sequence(merged_seq_file)
