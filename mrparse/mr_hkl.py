011a5183df38d8b0d13ad232aa4c18859cd69710
hlasimpk <hlasimpk@liverpool.ac.uk>
2020-09-11 11:10:28 +0100

    Removed SIMBAD dependency to fix multiprocessing issue - will try and change this back at some point

--- mrparse/mr_hkl.py
+++ mrparse/mr_hkl.py
@@ -9,7 +9,146 @@ from ample.util.ample_util import filename_append
 from cctbx.crystal import symmetry
 from mmtbx.scaling.matthews import matthews_rupp
 from mrbump.ccp4.MRBUMP_ctruncate import Ctruncate
-from simbad.util import mtz_util
+
+from iotbx import reflection_file_reader
+from iotbx.reflection_file_utils import looks_like_r_free_flags_info
+
+
+class GetLabels(object):
+    """Class to get the column labels for input mtz file
+    Attributes
+    ----------
+    f : str
+        f column label
+    sigf : str
+        fp column label
+    i : str
+        i column label
+    sigi : str
+        sigi column label
+    fplus : str
+        f(+) column label
+    sigfplus : str
+        sigf(+) column label
+    fminus : str
+        f(-) column label
+    sigfminus : str
+        sigf(-) column label
+    iplus : str
+        i(+) column label
+    sigiplus : str
+        sigi{+} column label
+    iminus : str
+        i(-) column label
+    sigiminus : str
+        sigi(-) column label
+    dano : str
+        dano column label
+    sigdano : str
+        sigdano column label
+    free : str
+        free column label
+    """
+
+    def __init__(self, mtz_file):
+        self.f = None
+        self.sigf = None
+        self.i = None
+        self.sigi = None
+        self.fplus = None
+        self.sigfplus = None
+        self.fminus = None
+        self.sigfminus = None
+        self.iplus = None
+        self.sigiplus = None
+        self.iminus = None
+        self.sigiminus = None
+        self.dano = None
+        self.sigdano = None
+        self.free = None
+
+        self.run(mtz_file)
+
+    def run(self, mtz_file):
+        reflection_file = reflection_file_reader.any_reflection_file(file_name=mtz_file)
+        if not reflection_file.file_type() == "ccp4_mtz":
+            msg = "File is not of type ccp4_mtz: {0}".format(mtz_file)
+            LOG.critical(msg)
+            raise RuntimeError(msg)
+
+        miller_arrays = reflection_file.as_miller_arrays()
+
+        for m_a in miller_arrays:
+            if looks_like_r_free_flags_info(m_a.info()) and not self.free:
+                self.free = m_a.info().labels[0]
+            elif self.check_anomalous(m_a):
+                if self.check_for_dano_labels(m_a):
+                    if len(m_a.info().labels) == 5:
+                        self.f, self.sigf, self.dano, self.sigdano, isym = m_a.info().labels
+                    elif len(m_a.info().labels) == 4:
+                        self.f, self.sigf, self.dano, self.sigdano = m_a.info().labels
+                    elif len(m_a.info().labels) == 2:
+                        self.dano, self.sigdano = m_a.info().labels
+                    else:
+                        LOG.debug("Unexpected number of columns found in anomalous miller array")
+                elif self.check_for_plus_minus_labels(m_a):
+                    if m_a.is_xray_amplitude_array():
+                        self.fplus, self.sigfplus, self.fminus, self.sigfminus = m_a.info().labels
+                    elif m_a.is_xray_intensity_array():
+                        self.iplus, self.sigiplus, self.iminus, self.sigiminus = m_a.info().labels
+                    else:
+                        LOG.debug("Type of anomalous miller array unknown")
+                else:
+                    LOG.debug("Type of anomalous miller array unknown")
+            elif m_a.is_xray_intensity_array() and len(m_a.info().labels) == 2 and not self.i:
+                self.i, self.sigi = m_a.info().labels
+            elif m_a.is_xray_amplitude_array() and len(m_a.info().labels) == 2 and not self.f:
+                self.f, self.sigf = m_a.info().labels
+
+    def check_anomalous(self, miller_array):
+        if miller_array.anomalous_flag():
+            return True
+        elif miller_array.info().type_hints_from_file == "anomalous_difference":
+            return True
+        # Check for anomalous miller arrays which aren't properly labeled
+        elif self.check_for_dano_labels(miller_array):
+            return True
+        elif self.check_for_plus_minus_labels(miller_array):
+            return True
+        return False
+
+    @staticmethod
+    def check_for_dano_labels(miller_array):
+        return any(["DANO" in i.upper() or "DP" == i.upper() for i in miller_array.info().labels])
+
+    @staticmethod
+    def check_for_plus_minus_labels(miller_array):
+        return any(["(+)" in i for i in miller_array.info().labels])
+
+
+def crystal_data(mtz_file):
+    """Set crystallographic parameters from mtz file
+    Parameters
+    ----------
+    mtz_file : str
+       The path to the mtz file
+    Returns
+    -------
+    space_group : str
+       The space group
+    resolution : str
+       The resolution
+    cell_parameters : tuple
+       The cell parameters
+    """
+
+    reflection_file = reflection_file_reader.any_reflection_file(file_name=mtz_file)
+    content = reflection_file.file_content()
+    space_group = content.space_group_name().replace(" ", "")
+    resolution = content.max_min_resolution()[1]
+    cell_parameters = content.crystals()[0].unit_cell_parameters()
+    return space_group, resolution, cell_parameters
+
 
 class HklInfo(object):
     def __init__(self, hklin, seq_info=None):
@@ -18,8 +157,8 @@ class HklInfo(object):
         if not os.path.isfile(hklin):
             raise RuntimeError("Cannot find hklin file: %s" % hklin)
         self.name = os.path.splitext(os.path.basename(hklin))[0]
-        self.labels = mtz_util.GetLabels(hklin)
-        self.space_group, self.resolution, self.cell_parameters = mtz_util.crystal_data(self.hklin)
+        self.labels = GetLabels(hklin)
+        self.space_group, self.resolution, self.cell_parameters = crystal_data(self.hklin)
         self.predicted_solvent_content = None
         self.predicted_ncopies = None
         self.molecular_weight = None
