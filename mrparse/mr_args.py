f59f26d7ee001ec9a6a3bb4c76384fccb789cbb0
hlasimpk <hlasimpk@liverpool.ac.uk>
2021-08-11 16:24:21 +0100

    Added config file and edited mr_args to accept it

--- mrparse/mr_args.py
+++ mrparse/mr_args.py
@@ -1,4 +1,5 @@
 import argparse
+import ConfigParser
 import os
 
 from mrparse.mr_version import __version__
@@ -17,12 +18,37 @@ class FilePathAction(argparse.Action):
 
 
 def parse_command_line():
+
+    # Read config file
+    config_file = os.path.join(os.environ["CCP4"], "share", "mrparse", "data", "mrparse.config")
+    defaults = {}
+    config = ConfigParser.SafeConfigParser()
+    config.read(config_file)
+    defaults.update(dict(config.items("Defaults")))
+    defaults.update(dict(config.items("Executables")))
+
+    # Read command line arguments
     parser = argparse.ArgumentParser(description="MrParse Molecular Replacement Search Model analysis")
-    parser.add_argument('--hklin', action=FilePathAction, help='MTZ/CIF Crystal Data file')
+    parser.set_defaults(**defaults)
+    parser.add_argument('-hkl', '--hklin', action=FilePathAction, help='MTZ/CIF Crystal Data file')
     parser.add_argument('--do_classify', action='store_true', help='Run the SS/TM/CC classifiers - requires internet access.')
     parser.add_argument('--pdb_dir', action=FilePathAction, help='Directory of PDB files')
     parser.add_argument('--db_lvl', help='Redundancy level of PDB database', default='95', choices=['95', '100'])
     parser.add_argument('--run_serial', action='store_true', help='Run on a single processor')
-    parser.add_argument('--seqin', action=FilePathAction, required=True, help='Sequence file')
+    parser.add_argument('-seq', '--seqin', action=FilePathAction, required=True, help='Sequence file')
+    parser.add_argument('--tmhmm_exe', action=FilePathAction,
+                        help="Location of TMHMM executable for transmembrane classification")
+    parser.add_argument('--deepcoil_exe', action=FilePathAction,
+                        help="Location of Deepcoil executable for coiled-coil classification")
     parser.add_argument('-v', '--version', action='version', version='%(prog)s version: ' + __version__)
-    return parser.parse_args()
+    args = parser.parse_args()
+
+    # Add tmhmm & deepcoil exe to config file so that it only needs to be specified once
+    if args.tmhmm_exe != "tmhmm":
+        config.set('Executables', 'tmhmm_exe', args.tmhmm_exe)
+    if args.deepcoil_exe != "deepcoil":
+        config.set('Executables', 'deepcoil_exe', args.deepcoil_exe)
+    with open(config_file, 'w') as f:
+        config.write(f)
+
+    return args
