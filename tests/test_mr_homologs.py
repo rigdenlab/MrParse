614ae166fb03f149b1b35c05e78b4ad63a415137
hlasimpk <hlasimpk@liverpool.ac.uk>
2021-08-11 13:44:14 +0100

    Add in af2 fasta file

--- tests/test_mr_homologs.py
+++ tests/test_mr_homologs.py
@@ -9,12 +9,13 @@ from mrparse.mr_homolog import homologs_from_hits, calculate_ellg, ellg_data_fro
 
 logging.basicConfig(level=logging.DEBUG)
 
+
 def test_2uvoA_homologs(test_data, get_2uvo_test_hits):
     hits = get_2uvo_test_hits
     homologs = homologs_from_hits(hits, pdb_dir=test_data.pdb_dir)
     h = homologs['1iqb_B_1']
     assert h.length == 77
-    assert abs(h.molecular_weight - 8601.5597) < 0.001
+    assert abs(h.molecular_weight - 8601) < 0.001
     assert h.pdb_id == '1iqb'
     assert h.resolution == 1.90
     chain_id = 'B'
@@ -29,7 +30,7 @@ def test_2uvoA_homologs_ellg(test_data, get_2uvo_test_hits):
     homologs = homologs_from_hits(hits, pdb_dir=test_data.pdb_dir)
     h = homologs['1iqb_B_1']
     assert h.length == 77
-    assert abs(h.molecular_weight - 8601.5597) < 0.001
+    assert abs(h.molecular_weight - 8601) < 0.001
     
     seq_info = Sequence(test_data.x2uvoA_fasta)
     hkl_info = HklInfo(test_data.x2uvo_mtz, seq_info=seq_info)
@@ -43,7 +44,7 @@ def test_phaser_log_ellg(test_data, get_2uvo_test_hits):
     ellg_data = ellg_data_from_phaser_log(test_data.phaser_log, homologs)
     assert ellg_data['2x3t_C_1'].ellg == 1378.4
     assert ellg_data['2x3t_C_1'].length == 168
-    assert ellg_data['2x3t_C_1'].molecular_weight == pytest.approx(16483.31)
+    assert ellg_data['2x3t_C_1'].molecular_weight == pytest.approx(16483)
 
 
 if __name__ == '__main__':
